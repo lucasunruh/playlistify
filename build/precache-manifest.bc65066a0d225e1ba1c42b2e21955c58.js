f190be97246748d438d4b2ec6b98b4d71335fc2c
Lucas Unruh <lucasdunruh@gmail.com>
2020-06-07 14:14:34 -0400

    Deploy test
    Testing deployment without gh-pages build command.

---
+++ build/precache-manifest.bc65066a0d225e1ba1c42b2e21955c58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc37333adeafcfa5e104a7b082fbf9af",
+    "url": "/playlistify/index.html"
+  },
+  {
+    "revision": "18789fec724bf7a5578d",
+    "url": "/playlistify/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a44e88e9e3e6e903fbe4",
+    "url": "/playlistify/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "18789fec724bf7a5578d",
+    "url": "/playlistify/static/js/2.d3870ee1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/playlistify/static/js/2.d3870ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a44e88e9e3e6e903fbe4",
+    "url": "/playlistify/static/js/main.7b7c97ba.chunk.js"
+  },
+  {
+    "revision": "40dd2f51c23acb32aae6",
+    "url": "/playlistify/static/js/runtime-main.ce0757be.js"
+  }
+]);
