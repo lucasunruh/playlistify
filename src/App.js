1f55ea524d78f064e56b6aeb449e6c398476f04d
Lucas Unruh <lucasdunruh@gmail.com>
2020-06-28 18:00:26 -0400

    Basic axios requests
    Added basic get/add playlist axios requests

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import React from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App () {
   return (
-    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-      <a class='navbar-brand' href='#'>Playlistify</a>
-      <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-        <span class='navbar-toggler-icon' />
-      </button>
+    <>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <a className='navbar-brand' href='#'>Playlistify</a>
+        <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon' />
+        </button>
 
-      <div class='collapse navbar-collapse' id='navbarSupportedContent'>
-        <ul class='navbar-nav mr-auto'>
-          <li class='nav-item active'>
-            <a class='nav-link' href='#'>Home <span class='sr-only'>(current)</span></a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav mr-auto'>
+            <li className='nav-item active'>
+              <a className='nav-link' href='#'>Home <span className='sr-only'>(current)</span></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <button className='btn btn-primary' onClick={
+        () => {
+          fetch('https://agile-brook-03586.herokuapp.com/')
+            .then(res => res.text())
+            .then(data => console.log(data))
+        }
+      }>
+        Request
+      </button>
+      <button className='btn btn-primary' onClick={
+        () => {
+          axios.get('https://agile-brook-03586.herokuapp.com/playlists')
+            .then(res => console.log(res.data))
+        }
+      }>
+        Get Playlists
+      </button>
+      <input class='form-control' type='text' onChange={
+        (e) => {
+          axios.post('https://agile-brook-03586.herokuapp.com/playlists/add', {
+            'playlist_name': e.target.value
+          })
+            .then(res => {
+              console.log(res)
+              console.log(res.data)
+            })
+        }
+      } />
+    </>
   )
 }
 
